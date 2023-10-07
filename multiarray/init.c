d03456469a3bbef0ead81f8e4a4ad846d04a44c1
erick <erickppedrada1@outlook.com>
2023-10-07 13:57:19 -0300

    init.c update

--- multiarray/init.c
+++ multiarray/init.c
@@ -4,20 +4,38 @@
 #include "arrayobj.h"
 #include "arrayprint.h"
 #include "arrayindex.h"
-
+#include "arraydims.h"
+#include "converts.h"
+#include "arrayinis.h"
 
 static const luaL_Reg funs[] = {
     {"newNdarray", l_newNdarray},
     {"arrayprint", l_arrayprint},
     {"arrayindex", l_arrayindex},
+    {"reshape", l_reshape},
+    {"transpose", l_transpose},
+    {"flatten", l_flatten},
+    {"tostring", l_ndarraytostring},
+    {"full", l_full},
+    {"zeros", l_zeros},
+    {"array", l_ndarray_by_table},
     {NULL, NULL}
 };
 
-int luaopen_init(lua_State *L){
+static const luaL_Reg methods[] = {
+    {"__tostring", l_ndarraytostring},
+    {"select", l_arrayindex},
+    {"print", l_arrayprint},
+    {NULL, NULL}
+};
+
+int luaopen_cinit(lua_State *L){
     luaL_newmetatable(L, "ndarray");
     lua_pushstring(L, "__index");
     lua_pushvalue(L, -2);
     lua_settable(L, -3);
+
+    luaL_setfuncs(L, methods, 0);
 
     luaL_newlib(L, funs);
     return 1;
