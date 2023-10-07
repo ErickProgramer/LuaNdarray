c553deb9810ad6883181f6f351d661ca2d5f6260
erick <erickppedrada1@outlook.com>
2023-10-07 13:50:40 -0300

    huge line of comments removed and repeated includes also removed, and explanation about the file added

--- multiarray/converts.c
+++ multiarray/converts.c
@@ -4,62 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// void c_ndarraytostring(char *res, Ndarray *arr, size_t indent_level,
-//                        const char *prefix, const char *sufix){
-//     if(arr->nd == 0){
-//         sprintf(res, "%g", *arr->data);
-//     } else {
-//         size_t i, j;
-//         Ndarray *aux;
-//         strcat(res, prefix);
-//         for(i = 0; i < arr->dimensions[0]; i++){
-//             aux = c_arrayindex(arr, i);
-//             if(aux->nd == 0){
-//                 char sprintf_res[20];
-//                 snprintf(sprintf_res, 20, "%g\0", *aux->data);
-//                 strcat(res, sprintf_res);
-
-//                 if(i < arr->dimensions[0]-1)
-//                     strcat(res, "\t");
-//             } else {
-//                 if(i > 0){
-//                     for(j = 0; j < indent_level; j++){
-//                         strcat(res, " ");
-//                     }
-//                 }
-
-//                 c_ndarraytostring(res, aux, indent_level+1, prefix, sufix);
-
-//                 if(i < arr->dimensions[0]-1){
-//                     for(j = 0; j < aux->nd; j++){
-//                         strcat(res, "\n");
-//                     }
-//                 }
-//             }
-//         }
-//         strcat(res, sufix);
-//     }
-// }
-
-// int l_ndarraytostring(lua_State *L){
-//     Ndarray *arr = (Ndarray*)luaL_checkudata(L, 1, "ndarray");
-
-//     // char res[1001];
-//     char *res = malloc(sizeof(char) * (arr->size * 10) + 1);
-//     printf("%u\n",sizeof(char) * arr->size + 1);
-//     c_ndarraytostring(res, arr, 1, "{", "}");
-//     lua_pushstring(L, res);
-
-//     return 1;
-// }
-
-#include "converts.h"
-#include "arrayindex.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <stdarg.h>
