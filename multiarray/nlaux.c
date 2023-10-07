87f1fa4513422660a9293c8c782bfc32bcb235a9
erick <erickppedrada1@outlook.com>
2023-10-07 13:56:11 -0300

    nlaux more functions

--- multiarray/nlaux.c
+++ multiarray/nlaux.c
@@ -2,9 +2,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // repeats a printf
 void nL_repprint(const char *txt, size_t q){
     for(size_t i = 0; i < q; i++)
         printf(txt);
+}
+
+size_t nL_getmaxalign(double *data, size_t data_len){
+    char *str = NULL;
+    sprintf(str, "%g", *data);
+
+    size_t len, max = strlen(str);
+    for(size_t i = 1; i < data_len; i++){
+        sprintf(str, "%g", data[i]);
+        len = strlen(str);
+        if(len > max) max = len;
+    }
+
+    return max;
 }
