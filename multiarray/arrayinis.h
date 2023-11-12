a55653f4df5079f871a1f4001b56e65312f05ad1
erick <erickppedrada1@outlook.com>
2023-11-12 18:07:05 -0300

    arrayinis update

--- multiarray/arrayinis.h
+++ multiarray/arrayinis.h
@@ -6,12 +6,8 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-int l_full(lua_State*);
-int l_zeros(lua_State*);
-int l_ones(lua_State*);
-int l_ndarray_by_table(lua_State*);
-int table_flatten(lua_State*);
-int aux_GetTableShape(lua_State*);
-int aux_flatten(lua_State*);
+int l_full(lua_State *L);
+int l_zeros(lua_State *L);
+int l_ones(lua_State *L);
 
 #endif /* LUANDARRAY_MULTIARRAY_ARRAYINIS_H_ */
